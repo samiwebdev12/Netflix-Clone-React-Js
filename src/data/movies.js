454dfb04cf212108262023096272fb98f5320a72
samiwebdev12 <samiwebdev12@gmail.com>
2026-09-07 10:15:46 +0500

    Initial commit: Nettflix React clone

---
+++ src/data/movies.js
@@ -0,0 +1,152 @@
+export const trendingMovies = [
+  {
+    id: 1,
+    title: "THE GENTLEMEN",
+    age: "18",
+    type: "Series",
+    year: "2026",
+    desc: "Aristocratic Eddie transitions from duke to drug baron when he inherits a weed empire — and partners with a criminal dynasty that has its own agenda.",
+    genres: "Comedies, Action, Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABc7e6rwQFsNGCsH1TIlwIXN3R9ZCM5OpDWGt_7pwSDptuOrf-0qzA52lVjrL4Dm7Fn1hcjKNbkfW5kFP3mfYumMzUWnX5qJt6_UFAZqME707ouc3hn8yG_j48FbRSFnE9T6I.webp?r=7fc",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWnRQ3_SFVSTMevK_0sXXbtNThkvg9j-zbEZ6z3eFViOhth27KJlP54_-gDKbqlWBw5yYbFrNUYQg2UweR2VBSA9u-KqawbZVKqq.webp?r=6c9",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABSnk22wowrU7hL6No2CfSh4YETMR0JGIxWxVPXUKu9NPBFFb0lrydOfcuHoRKqHobJz5R7vjxKXicbvrHYaq2gTJ0VRAQAL4Qg.webp?r=ba1",
+  },
+  {
+    id: 2,
+    title: "BEAUTY IN BLACK",
+    age: "18",
+    type: "Series",
+    year: "2026",
+    desc: "An exotic dancer's fate takes a turn when she crosses paths with the dysfunctional family behind a cosmetics dynasty and a devious trafficking scheme.",
+    genres: "Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABSbzJ6NnBf6p7bSm2F_WIJsR94io_SwDrox_Ebc56UAvSfkRmuK5T50HJ9BIc2cZbUaZGpyrXQ4RIt4TzwQnpXePRG0hkFD_yRAiB_6nTcPqJrTPa2NJnLUTPXfMklER9Y-l.webp?r=63e",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABb7EXKAHjzpfRUL_b35W6Ycz4OF3N8TGIdM9CnH31Sx9WHx9kVBhHnGCBh8V7I4Rxvv3t6fqqN-PWHW-yurupu2WcJsLgdjKZHDj.webp?r=b09",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABY7yso3rXOQvrRuL1ZJ29zMr3XiK7ebw-7RUmQdz6zlM-WJMjbJfV3mJcn_Nj31PQjqDjj_a73wIb3p11NcKHChkOhHJJYMZrg.webp?r=f3e",
+  },
+  {
+    id: 3,
+    title: "THE WHISPER MAN",
+    age: "15",
+    type: "Film",
+    year: "2026",
+    desc: "When his young son vanishes, a widower enlists help from his estranged father, a retired detective who put away the serial killer now linked to the case.",
+    genres: "Thrillers, Mysteries, Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABT-H3ThjtotPgF63a7t-hPXglEUTHS9Ru3AvQLRr5UJ-s39yL-pNspyafoSzaaUhopGDn27QMhyFUDUGVI7RTYkGkNQu7YccP48xhCD_5c2OH4ePtbi6VyS4_C8rDWZwynBP.webp?r=649",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABeYPnd8CY1FsRcNWHqta3ssLWopa92Gf8LeHJ9-wPoCT-lunOZL-xgP3CAOzFyuKq-ir9m4pUY5esngNj2uVKP6OWAumasg6LBxF.webp?r=7ca",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABVFiNPe-4p4qK3Urb0II7O_e0VaawVHVgMeicHSoVwTsaos4QfaQ6aOv-OUeDinF-Qrzw5oNyYBWdEElgY7N8ahMdwoa1Pr1AQ.webp?r=813",
+  },
+  {
+    id: 4,
+    title: "GRAND THEFT AUTO VI",
+    age: "18",
+    type: "Film",
+    year: "2026",
+    desc: "See the next evolution in the groundbreaking Grand Theft Auto series. Coming November 19 to PlayStation 5 and XBOX Series X|S.",
+    genres: "Action, Thrillers, Adventure",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABZSCelQ5pahMlV7dR3yh0Sc7tnxT0P4JF35sdHJ4C6oPn7JD1d2KJRMHQbo-xzjkjylmBnManMMPXvavZ1KizJyI4fcZTweuVXE.webp?r=367",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABZR_PlThmancVYD88a5IYI7ZIRRgGfN2v1DCR86j9us7aTq8miQxauV1ueFJu7zAlEndor2_pvl4xAnAwq2QrvLhviauTW-Fk7fm.webp?r=285",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABYRXJiCV2Yazes_iNHiOoBjLfToiX5jELqkAQ5JRFXatgmBKKE28TScCFwMjokkCeo04TSzxrWoAVKYB4V6dZl4eu55RrTSy-A.webp?r=94c",
+  },
+  {
+    id: 5,
+    title: "DEATH OF THE PASTOR'S WIFE",
+    age: "15",
+    type: "Series",
+    year: "2026",
+    desc: "What happened to Mica Miller? This documentary series retraces her promising life, toxic marriage and the mysterious circumstances of her death.",
+    genres: "Documentaries",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABQxjdY3-o2_HMGPNScx63Fh6MJUriV48oMXIV1HbP5EFuoPzSzj24paH4cP9mVN3YHh71oRS_5GfDV5I5Exl-Tv_DlRWSmE4JiofsQDkIrlT6b53N_vJD8lI7qjEUCsJCTRP.webp?r=bdf",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUiW0troxGFhVNF_kZ0PybCtczp8zrYsrq_EMjmrs9wjwHG27MzxooPKNbvs167_0F0HDFyACOD3AeSKTfQE3_81bzkvbKzZp9qc.webp?r=81b",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABTbA7cU28kDW_ZwOW50yuvI-lB1XckUwr1oy0ziX-lltiehR95o5qf4BbLqPfzqYsL_v1zoMMLvCP2it8fEp4RThp9aa0Hr6Iw.webp?r=4de",
+  },
+  {
+    id: 6,
+    title: "OUTER BANKS",
+    age: "15",
+    type: "Series",
+    year: "2026",
+    desc: "On a North Carolina island of haves and have-nots, John B and his tight-knit crew of friends find mystery and adventure while hunting for lost treasure.",
+    genres: "Adventure, Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABQaZz6w4WN-xljuaOiyyPIV2iwoXVJyvgdJu-AvWezdhexhlDUNxIZmg8fk2m7ValjyQ563IfsRtuKYh5UhD6ovWtfNAVdNfkcc_2mKttwIg1F3H_6YFxWpRd6lS_F7mOorK.webp?r=279",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABT1mO6_e7GqT0bW2BTNdVSoKEk36EMCnTyHT1q7tFhqko7OvVMhYxHqfrgaL45oNoPTxpZcgw9-Pv1siIYG_iQjcREuutT_ZygYi.webp?r=9ee",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABQ2blB-qPFUf7CxS20fG8ZM1lXrMoMjLkZIH0pGWNshsZHoWIydlAuzfiEIY0aowlmjcSxJWxZRWvdEMg1dsdqS0RsEtMUTCcg.webp?r=337",
+  },
+  {
+    id: 7,
+    title: "LOVE IS BLIND: UK",
+    age: "15",
+    type: "Series",
+    year: "2026",
+    desc: "Emma and Matt Willis host a social experiment where British singles look for love and get engaged before meeting in person. But who will say \"I do\"?",
+    genres: "Reality Programming",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABejD-g6lnKw0x6cxBkKu9CLeA5uCkBonlgN_ey_78qBKNfNnQgnyyajO0RH3jXmhUZTQGSZ3VP1wYefA03oOD9tx7mg3v3lAKAJQdC1SXGsekqZAVJCHh8MiKqGsUaxOL8Su.webp?r=f87",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABUHD9o52xob--hohFTF0gmh7EgnzJuu2YdskY25ML-gLtseHsAlvRK9mf25QW4vez1A7OHjpGe5E5qKYPrFBN-Mu_rcf3VlJOqit.webp?r=9b2",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABZ0sw21aNvudLC6ykO0PyAnyh3yF3oc4bskbz0OukpwdLdiWFumwcJQeEl2TGr76rFmenFQOHXLYJpp6G9NzQF6y9m_hxIrfLg.webp?r=60b",
+  },
+  {
+    id: 8,
+    title: "THE LAST HOUSE",
+    age: "12",
+    type: "Film",
+    year: "2026",
+    desc: "A family mysteriously sealed inside their home must work together to survive dwindling resources and the ominous force keeping them trapped.",
+    genres: "Horror, Thrillers, Sci-Fi",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABeMf38J8BfSwaPGwqyyH1bh9xRy5TTduGHkgRHOrPAWSQV2qLqJOyOPgTWpQS_X8sm08WbnzcHtnEWSF0lym_O6wWU6FSapo7A3VQelXKzOKywdsjcpFeuDxpeCvkZo95tyG.webp?r=a45",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABWPLbcJDrENGB2vQfgzgXx2HcGNH3rivWLA7ZII3paJQBJfx4OwmzYHveLQw9zsDIvL5cQZzxTdQLpCXLAzOe8VSq_m6z_VlQjwI.webp?r=4c2",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABbOl130ggIfdeuNWT5U1yZkdZAvzG-UERPkSx8aEGDO_zv6b_-Jx75A8UsX2mbhUhHJjBVOl1_brSd2UOx7i7NRarZS3PlOR9g.webp?r=9a8",
+  },
+  {
+    id: 9,
+    title: "I WILL FIND YOU",
+    age: "15",
+    type: "Series",
+    year: "2026",
+    desc: "Wrongfully imprisoned for murdering his own son, a father sets out on a desperate rescue mission after learning the boy may still be alive.",
+    genres: "Thrillers, Mysteries, Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABUI_HDeoNxLWGvwc5JDGwCXievUU0XiDZowIQJrt0kU8gxOHfsHIY7NfNVXZER7UqW5AuZ2J0wwGk3IJQ69nHRj7Rl1hLdE910Nv_yFYnNafv5SmfSnuJek-W8L2UBOfHHHb.webp?r=c16",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABZo7-GwjXpTQFTX77OHk3b6pP6CX21aHPwDzbO_z1RFbhHlFeWXxthB9t64sb3vKVyf94q9aVRackURjl97yGAHoxCt_6AHkbNtc.webp?r=68d",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABayGryus3wL4EChCT9lv6hDBu5M_yILzeu__Hp2ZgwrcajEUFfM6cEby-0Lx1QXDFB43jS8YsGTu2RtPY3qLc__NnIMwp03zvg.webp?r=b6c",
+  },
+  {
+    id: 10,
+    title: "MY LIFE WITH THE WALTER BOYS",
+    age: "12",
+    type: "Series",
+    year: "2026",
+    desc: "After a tragic loss, a teen adapts to life with her guardian's big family in a small town, where she learns new lessons about love, hope and friendship.",
+    genres: "Romance, Dramas",
+    image:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/mAcAr9TxZIVbINe88xb3Teg5_OA/AAAABWQyMQrfAxmOVXl8iBm1wxIZDCRuFnfMvAW6wpAaI0IiOOYi0eZ5skWATnUT787Z3aODhgV7SJmcIimwwoLWVD1OT8PPSwV4jQAGCflznrwJBh3SAlr-HrOt_pBhcJn_ck2y.webp?r=dbc",
+    cover:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/6AYY37jfdO6hpXcMjf9Yu5cnmO0/AAAABSK7D80z9lzKbKBkVdzVsMAaaXXNxcqtzgcDAJNeW8oCwnlLypSckF_OdHJWHuW40v5OTkWxe1c4wWOj_i1LSHi1RGe4XtRc83L2.webp?r=775",
+    titleImg:
+      "https://occ-0-1167-1168.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABV0j7JgZLelEKuAPd-847JsTVuY05_AWU7G4Mdaglj-yQkwqNbjFcFMiNwet9HaT38pD6OcmHv_KnX_t_GtWz8RFbSItSgVqyQ.webp?r=067",
+  },
+];
